645edfe64fdc162e44a4e396ba5b92eb2172ceca
Tori <143439237+vdouglasSound@users.noreply.github.com>
2024-02-26 17:46:42 +0000

    adding files

---
+++ myStatements.py
@@ -0,0 +1,8 @@
+my_list = ["apple", "cherry", "lemon"]
+my_dict = {"drink": "coffee", "milk": "whole"}
+
+for item in my_list:
+    print(f"My favorite fruit is: {item}")
+
+for _,value in my_dict.items():
+    print(f"My favorite thing to drink is: {value}")
